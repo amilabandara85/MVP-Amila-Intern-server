dd0303b699c31372c7bb4668696f26defe163683
Amila Madushanka Mapa Bandara <amilabandara8m@gmail.com>
2025-11-20 09:41:18 +1300

    Add files via upload

---
+++ Controllers/SalessController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AmilaOnboarding.Server.Models;
+
+namespace AmilaOnboarding.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SalessController : ControllerBase
+    {
+        private readonly AmilaOnboardingContext _context;
+
+        public SalessController(AmilaOnboardingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Saless
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sale>>> GetSales()
+        {
+            try
+            {
+
+                return await _context.Sales
+                    .Include(s => s.Customer)
+                    .Include(s => s.Product)
+                    .Include(s => s.Store)
+                    .ToArrayAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving sales data.");
+            }
+
+        }
+
+        // GET: api/Saless/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sale>> GetSale(int id)
+        {
+            try
+            {
+                var sale = await _context.Sales
+                .Include(s => s.Customer)
+                .Include(s => s.Product)
+                .Include(s => s.Store)
+                .FirstOrDefaultAsync(s => s.Id == id);
+
+                if (sale == null)
+                {
+                    return NotFound("This record not in your DB");
+                }
+
+                return sale;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred while retrieving the sale record.");
+            }
+        }
+
+        // PUT: api/Saless/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSale(int id, Sale sale)
+        {
+            if (id != sale.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sale).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SaleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Saless
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Sale>> PostSale(Sale sale)
+        {
+            _context.Sales.Add(sale);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSale", new { id = sale.Id }, sale);
+        }
+
+        // DELETE: api/Saless/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSale(int id)
+        {
+            var sale = await _context.Sales.FindAsync(id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sales.Remove(sale);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SaleExists(int id)
+        {
+            return _context.Sales.Any(e => e.Id == id);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,47 @@
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
 using AmilaOnboarding.Server.Models;
+using System.Text.Json.Serialization;
 
 
+// Define the name for your CORS policy outside of the main configuration block
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins"; // <-- NEW LINE
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// 1. ADD CORS SERVICES
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            // IMPORTANT: Replace http://localhost:5173 with your actual React/Vite development server URL if it's different.
+            policy.WithOrigins("http://localhost:5173",
+                "https://amila-mvponboarding-web.azurewebsites.net")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+}); // <-- NEW BLOCK END
+
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    //options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    //options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
 });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AmilaOnboardingContext>(Option =>
-    Option.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Initial Catalog=Amila-Onboarding;Integrated Security=True;TrustServerCertificate=Yes"));
+    Option.UseSqlServer(builder.Configuration.GetConnectionString("AzureSQLConnection")));
+
+builder.Services.AddDbContext<AmilaOnboardingContext>(Option =>
+      Option.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Initial Catalog=Amila-Onboarding;Integrated Security=True;TrustServerCertificate=Yes"));
+     
 
 
 var app = builder.Build();
@@ -33,6 +57,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// 2. USE CORS MIDDLEWARE (MUST BE BEFORE app.UseAuthorization())
+app.UseCors(MyAllowSpecificOrigins); // <-- NEW LINE
 
 app.UseAuthorization();
 
