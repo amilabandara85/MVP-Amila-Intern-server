38d3cbfb17fe911d160c6dbce9e1736d63846cc0
Amila Madushanka Mapa Bandara <amilabandara8m@gmail.com>
2025-11-22 17:31:06 +1300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,56 @@
-using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
 using AmilaOnboarding.Server.Models;
 
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. ADD CORS SERVICES
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins(
+                "https://localhost:55677", // Local Server HTTPS
+                "https://localhost:55676", // Local Server HTTPS
+                "http://localhost:5173",   // Local Client (Vite) HTTP
+                "https://amila-mvponboarding-web.azurewebsites.net") // Azure Deployed Client/Server
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AmilaOnboardingContext>(Option =>
-    Option.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Initial Catalog=Amila-Onboarding;Integrated Security=True;TrustServerCertificate=Yes"));
+// Use the connection string from appsettings.json
 
+builder.Services.AddDbContext<AmilaOnboardingContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("AzureSQLConnection"),
+        sqlOptions =>
+        {
+            // Enable retry logic (better known as connection resiliency)
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 10,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null);
+        }));
+
+//builder.Services.AddDbContext<AmilaOnboardingContext>(Option =>
+//    Option.UseSqlServer(builder.Configuration.GetConnectionString("AzureSQLConnection"))); // CORRECTLY READS FROM appsettings.json
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -33,6 +58,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// 2. USE CORS MIDDLEWARE
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
